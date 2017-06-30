4cd25ac4d45cd00afba2ca578b2095f2840cb382
tianapa <teer_na_ka@hotmail.com>
2017-06-30 11:36:03 +0700

    add sign up function

--- calpetrol/views.py
+++ calpetrol/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+#from .forms import UserRegistrationForm
+from django import forms
 from .models import *
+from django.contrib.auth.models import User
 k_source = ''
 k_destination = ''
 k_car_type = ''
@@ -64,6 +69,20 @@ def calculate(request):
 'get_cost_petrol':get_cost_petrol,'cost':cost,'save_source':save_source,'save_destination':save_destination,'save_car_type':save_car_type,
 'save_cost_petrol':save_cost_petrol,'save_cost':save_cost})
 
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return render(request,"calpetrol/home1.html")
+    else:
+        form = UserCreationForm()
+    return render(request, 'calpetrol/signup.html', {'form': form})
 
 
 
