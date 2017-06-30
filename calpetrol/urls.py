f9c64e68c3419c475d70e674fd15f6740e3f8da6
tianapa <teer_na_ka@hotmail.com>
2017-06-30 18:45:23 +0700

    add url user_data

--- calpetrol/urls.py
+++ calpetrol/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^$', views.home_page, name='home'),
     url(r'^cal/$', views.calculate, name='cal'),
     url(r'^signup/$', views.signup, name='signup'),
+    url(r'^user_data/$', views.userdata, name='userdata'),
 ]
 
 
