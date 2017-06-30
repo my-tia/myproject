12c61b417a3ee7360c656fd5526057ba906a1978
tianapa <teer_na_ka@hotmail.com>
2017-06-30 11:36:56 +0700

    add sign up url

--- calpetrol/urls.py
+++ calpetrol/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
-
 from . import views
 
 app_name = 'calpetrol'
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
     url(r'^cal/$', views.calculate, name='cal'),
+    url(r'^signup/$', views.signup, name='signup'),
 ]
+
+
+ 
