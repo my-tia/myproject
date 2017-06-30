6b08344be2078b82904f0be584cd534b8bb83c70
tianapa <teer_na_ka@hotmail.com>
2017-06-30 18:40:41 +0700

    edit add user_data model

--- calpetrol/models.py
+++ calpetrol/models.py
@@ -1,36 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime
+
+class Distance(models.Model):
+    source_text = models.CharField(max_length=200)
+    destination_text = models.CharField(max_length=200)
+    distance = models.IntegerField(default=0)
+
+    def __str__(self):
+        return "%s %s" % (self.source_text, self.destination_text)
+        
+
+class Car(models.Model):
+    car_type = models.CharField(max_length=200)
+    car_use = models.IntegerField(default=0)
+    def __str__(self):
+        return self.car_type
+
+class User_data(models.Model):
+    user = models.ForeignKey(User,null=True)
+    source_destination = models.ForeignKey(Distance,null=True)
+    car = models.ForeignKey(Car,null=True)
+    petrol_cost = models.FloatField(default=0)
+    cost = models.FloatField(default=0)
+    date = models.DateField(("Date"), default=datetime.date.today)
 
 
-class Classification(models.Model):
+    def __unicode__(self):
+        return self.user
    
-    class_text = models.CharField(max_length=200,default='class_quiz')
-    #date = models.DateField(("Date"), default=datetime.date.today)
-    
-    def __str__(self):
-        return self.class_text
+
+# Create your models here.
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    choice_correct = models.CharField(max_length=200)
-    classification = models.ForeignKey(Classification, on_delete=models.CASCADE)
-    #date = models.DateField(("Date"), default=datetime.date.today)
-    pub_date = models.DateTimeField('date published')
-    def __str__(self):
-        return self.question_text
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    def __str__(self):
-        return self.choice_text
- 
-class Player(models.Model):
-    name_player = models.CharField(max_length=200)
-    score = models.IntegerField(default=0)
-    classification = models.ForeignKey(Classification, on_delete=models.CASCADE)
-    
-    def __str__(self):
-        return self.name_player
