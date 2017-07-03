6c411bd17fd77d28a3059f55f73c43ee543d4319
tianapa <teer_na_ka@hotmail.com>
2017-07-03 15:49:35 +0700

    Add files via upload
    update user story

--- functional_tests.py
+++ functional_tests.py
@@ -10,22 +10,47 @@ class NewVisitorTest(unittest.TestCase):
     def setUp(self):  
         self.browser = webdriver.Firefox()
 
-    def tearDown(self):  
-        self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):  
         #เพื่อนของทีเร็คได้แนะนำเว็บนึงที่น่าสนใจ เขาจึงลองเข้าไปตามที่อยู่เว็บที่ได้รับมาจากเพื่อน
-        self.browser.get('http://localhost:8000/calpetrol')
+        self.browser.get('http://localhost:8000/')
 
         #เขาเห็นแถบหน้าต่างเว็บนั้นเขียนไว้ว่า’calculate petrol for your car’ หัวเรื่อง ‘calculate petrol’
         self.assertIn('calculate petrol for your car', self.browser.title)  
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('คำนวณราคาน้ำมันระหว่างจังหวัดในประเทศไทย', header_text)
+        self.browser.find_element_by_id('start').click()
+        
+
+
+    def test_signup(self):
+        #เขายังไม่เคยเข้าระบบลงทะเบียน จึงคลิกไปที่ url ที่ลงทะเบียนใหม่
+        self.browser.get("http://localhost:8000/signup/")
+ 
+       #คลิกปุ่ม sign up แล้วหน้า urlจะไปที่หน้าแรกทันทีเมื่อลงเสร็จ
+        self.browser.find_element_by_id('signup').click()
+        self.assertIn("http://localhost:8000/", self.browser.current_url)
+
+    def test_login(self):
+        #หลังจากที่เขาได้ลงทะเบียนเสร็จ ก็ทำการlogin เข้าระบบต่อ
+        self.browser.get("http://localhost:8000/login/")
+        #self.driver.find_element_by_id('id_title').send_keys("test title")
+        #self.driver.find_element_by_id('id_body').send_keys("test body")
+       #คลิกปุ่ม login 
+        self.browser.find_element_by_id('login').click()
         self.fail('Finish the test!')  
 
-        #หน้าเว็บมีตัวเลือกอยู่สามอันโดยอันแรกเป็น source ,destination และ car type ตามลำดับ
+    def tearDown(self):  
+        self.browser.quit()       
+
+    def test_cal(self):
+        self.browser.get("http://localhost:8000/cal/")
+      #หน้าเว็บมีตัวเลือกอยู่สามอันโดยอันแรกเป็น source ,destination และ car type ตามลำดับ
 
         #เมื่อเขาเลือกเสร็จก็ใส่ค่าราคาน้ำมันลงไป
 
         #แล้วเขาก็เลือกปุ่มกด calculate
+        self.browser.find_element_by_id('cal').click()
 
 if __name__ == '__main__':  
     unittest.main(warnings='ignore') 
